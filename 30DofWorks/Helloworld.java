bd5872a5a3a1e31d2ce80515d9808cc572077f93
Terry <terryvilver@gmail.com>
2019-08-24 17:34:45 +0200

    ajout de Hello world

---
+++ 30DofWorks/Helloworld.java
@@ -0,0 +1,9 @@
+public class Helloworld{
+    public static void main(String []args){
+        
+        System.out.println("Hello world.");
+        System.out.print("Welcome to 30 Days of Code!");
+
+    }
+
+}
