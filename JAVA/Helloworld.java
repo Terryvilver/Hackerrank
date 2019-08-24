0165f2956a5bed0805062213d75993c330dd86b0
Terry <terryvilver@gmail.com>
2019-08-24 17:44:46 +0200

    Premier programme en java

---
+++ JAVA/Helloworld.java
@@ -0,0 +1,13 @@
+//https://www.hackerrank.com/challenges/welcome-to-java/problem
+
+//Ce programme a pour but de afficher du texte.
+public class Helloworld{
+
+    public static void main(String []args){
+
+        System.out.println("Hello, World.");
+        System.out.print("Hello, Java.");
+
+    }
+
+}
