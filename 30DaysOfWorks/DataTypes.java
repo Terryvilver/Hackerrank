6e7340ec809ea1bb902b82e5fdee844de9244b60
Terry <terryvilver@gmail.com>
2019-08-25 22:15:08 +0200

    ajout de Data Types

---
+++ 30DaysOfWorks/DataTypes.java
@@ -0,0 +1,28 @@
+//https://www.hackerrank.com/challenges/30-data-types/problem
+
+import java.util.Scanner;
+
+public class DataTypes{
+
+    public static void main(String []args){
+        int i = 12;
+        double d = 4.0d;
+        String s = "HackerRank ";
+    
+        Scanner sc = new Scanner(System.in);
+        int entier = sc.nextInt();
+        double flottant = sc.nextDouble(); 
+
+        sc.nextLine();
+
+        String texte = sc.nextLine();
+
+        System.out.print("\n");
+
+        System.out.println( i + entier);
+        System.out.println( d+ flottant);
+        System.out.print( s + texte);
+
+    }
+
+}
